72abe4bc99d629377b2874d0d0fe91b1cc95d4f4
wirtu0z <125866203+wirtu0z@users.noreply.github.com>
2023-03-31 16:46:47 +0300

    Add files via upload

---
+++ precache-manifest.cbd15764ae84b2a896a92b250fd2b3ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bfa9439b66ee03d940c0e69e26edfc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7843d7f3bcb232df7ad9",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "3a37c87d825212c9acf3",
+    "url": "/static/js/2.00fb7832.chunk.js"
+  },
+  {
+    "revision": "7843d7f3bcb232df7ad9",
+    "url": "/static/js/main.39f58dd8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
